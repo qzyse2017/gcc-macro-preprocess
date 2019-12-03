b5e4f73e0fb5891431d7a4561c4fde76ae57d6a7
zyck <zyck2019@outlook.com>
2019-12-03 11:09:44 +0800

    .

--- pre.sh
+++ pre.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 #
-
 pre_lines=$(gcc -E preprocess/pre_lib.c)
-# newlines=""
-# for i in $pre_lines;do
-#     if ![[${i} == "#*"]];then 
-#         $newlines=$newlines + $i +"\n"
-#         echo $i
-#     fi
-# done
 echo $pre_lines >> gen.go
-exit 0
